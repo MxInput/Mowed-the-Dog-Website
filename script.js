7a2f0d022c5dee766547e488cedad84cc378c3a7
MxInput <mxinput@protonmail.com>
2026-07-01 08:48:44 -0400

    adding new page

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
+var LCount = 0;
+var ICount = 1;
+var KCount = 2;
+var ECount = 3;
+
 function changeColors() {
-    document.getElementById("L").style.color = 
-    document.getElementById("I").style.color = 
-    document.getElementById("K").style.color = 
-    document.getElementById("E").style.color = 
+    var avaliableColors = ["#12786f", "#214191", "#cfbc0a", "#a33a17"]
+    
+    document.getElementById("L").style.color = avaliableColors[LCount];
+    document.getElementById("I").style.color = avaliableColors[ICount];
+    document.getElementById("K").style.color = avaliableColors[KCount];
+    document.getElementById("E").style.color = avaliableColors[ECount];
+
+    LCount++;
+    ICount++;
+    KCount++;
+    ECount++;
+
+    if (LCount > 3) {
+        LCount = 0;
+    }
+
+    if (ICount > 3) {
+        ICount = 0;
+    }
+
+    if (KCount > 3) {
+        KCount = 0;
+    }
+
+    if (ECount > 3) {
+        ECount = 0;
+    }
 }
 
+changeColors();
+
+setInterval(() => {
+  changeColors();
+}, 3000); 
+
+function resetNav() {
+    if (document.head.title == "Handhelds I Like") {
+        document.getElementById("controller_link").color = "#105041";
+    }
+    else {
+        document.getElementById("handheld_link").color = "#105041";
+    }
+}
