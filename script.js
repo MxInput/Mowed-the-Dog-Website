348db146b9a9738f3ba6c91e3297350c5f5c92bd
MxInput <mxinput@protonmail.com>
2026-06-30 21:18:02 -0400

    adding selection effects

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColors() {
+    document.getElementById("L").style.color = 
+    document.getElementById("I").style.color = 
+    document.getElementById("K").style.color = 
+    document.getElementById("E").style.color = 
+}
+
